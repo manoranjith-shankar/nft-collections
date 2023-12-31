402b94d68e699e252e4895ddfc6058567d30571e
manoranjith-shankar <21csbs023@skcet.ac.in>
2023-12-31 13:30:06 +0530

    [update]config

--- Readme.md
+++ Readme.md
@@ -1,2 +1,6 @@
 # Here goes the nft-collection dapp using whitelist
-[https://learnweb3.io/degrees/ethereum-developer-degree/sophomore/build-an-nft-collection-with-a-whitelist-using-hardhat-and-solidity]
+
+## clone and rename .env.local to .env and add the following 
+   - PRIVATE_KEY, 
+   - ETHERSCAN_API, 
+   - RPC_URL - for this test, i used buildbear polygon mainnet sandbox [https://rpc.buildbear.io/known-luminara-unduli-0a51af9f], you can use any RPC endpoint
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,19 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox-viem";
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config({ path: ".env" });
 
-const config: HardhatUserConfig = {
-  solidity: "0.8.19",
+const RPC_URL = process.env.RPC_URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
+
+module.exports = {
+  solidity: "0.8.18",
+  networks: {
+    buildBear: {
+      url: RPC_URL,
+      accounts: [PRIVATE_KEY],
+    },
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
+  },
 };
-
-export default config;
